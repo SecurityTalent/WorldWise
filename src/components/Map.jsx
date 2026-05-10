5450176d404b6e16de888647334ee1116c54ecd8
SecurityTalent <thebrandbox101@gmail.com>
2026-05-10 17:05:08 +0600

    Advanced State Management The Context API

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Map.module.css"
 import { useNavigate, useSearchParams } from 'react-router'
-
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 function Map() {
 
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+
+
 
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
@@ -16,13 +18,19 @@ function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={() => { navigate('form') }}>
-      <h1>Map</h1>
-      <h1>Position: {lat} || {lng}</h1>
-      <button
 
-        onClick={() => setSearchParams({ lat: "51.505", lng: "-0.09" })}
+      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
 
-      >Change Position</button>
     </div>
   )
 }
